6a1829b579617b37579334e2f0edb6881e4445f1
Abdullah Qabbani <mhamad50513@gmail.com>
2024-03-01 21:21:55 +0200

    Adding error file for macro all error types

---
+++ LIB/ERROR_TYPES.h
@@ -0,0 +1,27 @@
+
+/********************************************************************************
+ *	FILE DESCRIPTION
+ ********************************************************************************
+ * Filename              :   ERROR_TYPE.h
+ * Author                :   Abdullah Qabbani
+ * Origin Date           :   01/03/2024
+ * Version               :   1.0.0
+ * Compiler              :   GCC ARM Embedded Toolchain
+ * Target                :
+ * Notes                 :	
+ *
+ *********************************************************************************/
+/************************ SOURCE REVISION LOG *************************************
+ *
+ *  Date 	 	     Version   Author             Description
+ *  01/03/2024       1.0.0     Abdullah Qabbani   Initial Release.
+ *
+ **********************************************************************************/
+#ifndef ERROR_TYPE_H_
+#define ERROR_TYPE_H_
+
+#define OK        1u
+#define NOK       2u
+#define NULL_PTR  3u
+
+#endif
