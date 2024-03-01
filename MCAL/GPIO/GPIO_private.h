7dcea4d203f8b974205a350f38f74c4c1ef1d133
Abdullah Qabbani <mhamad50513@gmail.com>
2024-03-01 21:23:07 +0200

    Adding private header file for private macros and private function

---
+++ MCAL/GPIO/GPIO_private.h
@@ -0,0 +1,36 @@
+/**********************************************************************************
+ *	FILE DESCRIPTION
+ **********************************************************************************
+ * Filename              :   GPIO_private.h
+ * Author                :   Abdullah Qabbani
+ * Origin Date           :   1/03/2024
+ * Version               :   1.0.0
+ * Compiler              :   GCC ARM Embedded Toolchain
+ * Target                :   
+ * Notes                 :	
+ *
+ **********************************************************************************/
+/************************ SOURCE REVISION LOG *************************************
+ *
+ *  Date 	   		 Version   Author             Description
+ *  01/03/2024       1.0.0     Abdullah Qabbani   Initial Release.
+ *
+ **********************************************************************************/
+#ifndef GPIO_PRIVATE_H_
+#define GPIO_PRIVATE_H_
+
+#define TWO_PIN_ACCESS                  2u
+#define FOUR_PIN_ACCESS                 4u
+
+#define ONE_BIT_MASK                    0b1
+#define TWO_BIT_MASK                    0b11
+#define FOUR_BIT_MASK                   0b1111
+
+
+
+
+
+
+
+
+#endif
